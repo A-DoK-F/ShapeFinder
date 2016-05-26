33edc74ea391ca63455ad3f8d9ff0f6ae3eb3787
Loperiane <loperanes@gmail.com>
2016-05-26 16:11:36 +0200

    Delete connbdd.php

--- connbdd.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$hostname = "localhost";
-	$user     = "root";
-	$password = "pass";
-	$nom_base_donnees = "ShapeFinder";
-
-	$conn = mysql_connect($hostname, $user, $password) or die(mysql_error());
-?>
